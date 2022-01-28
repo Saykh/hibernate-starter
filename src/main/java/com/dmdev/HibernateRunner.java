ead6b1c9b9c8a8267f6562b85452cb75a6c73d0f
acer <edilov@mail.ru>
2022-01-28 16:26:05 +0300

    ManyToOne

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -64,15 +64,55 @@ public class HibernateRunner {
                     (3) Регистрируем в hibernate.cfg.xml, либо в нашем HibernateUtil классе.
                     (4) Сохраняем.
 
-
+                    session1.save(company);
+                    session1.save(user);
                 */
-                session1.save(company);
-                session1.save(user);
+
 
                 /*
                     В нашем PersistentContext будет две сущности.
+                */
 
-                    
+
+                User user1 = session1.get(User.class, 1L);
+
+
+                /*
+                    select
+
+                    from
+                    users user0_
+                    inner join
+                    company company1_
+                    on user0_.company_id=company1_.id
+                    where
+                    user0_.id=?
+
+                Поставили optional = false и добились inner join.
+
+                А как сделать отдельный запрос на получение компании? Мы к этому ещё вернемся на
+                проблеме n + 1.
+
+                Но делается это так.
+                Вот мы поставили fetch на Lazy. Но оп, теперь мы не видим запроса в company.
+                Мы не получили компанию. Что ж такое?
+                Смотрим дебаг. Компания установлена. Но мы видим не компанию, а какой-то Company$HibernateProxy....
+                Это прокси класс, который создается динамически во время выполения, и на этапе компиляции
+                его нет. Если открыть его, увидим, что он похож на обычный класс company, но ещё
+                с каким-то полем interceptor. Благодаря прокси и их реализации у нас есть возможность инициализ-ть
+                нашу компанию тогда, когда мы попросим поля у нашей сущности company.
+
+                Hibernate, используя proxy, позволяет нам лениво инициал-ть сущность, если мы попросим об этом.
+
+                https://habr.com/ru/company/otus/blog/578950/
+
+
+                Короче, через Lazy и Optional мы оптимизируем работу наших запросов.
+
+
+
+
+
                 */
 
 
